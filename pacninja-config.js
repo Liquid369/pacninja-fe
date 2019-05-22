6e2c5676a0b8dc88f34f7a98dae1196cf4f7a319
Akshay <akshaycm@hotmail.com>
2019-05-22 14:57:44 +0300

    [Config] Update to PAC

--- pacninja-config.js
+++ pacninja-config.js
@@ -1,19 +1,19 @@
 /*
- This file is part of Dash Ninja.
+ This file is part of Paccoin Ninja.
  https://github.com/akshaynexus/pacninja-fe
 
- Dash Ninja is free software: you can redistribute it and/or modify
+ Paccoin Ninja is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- Dash Ninja is distributed in the hope that it will be useful,
+ Paccoin Ninja is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with Dash Ninja.  If not, see <http://www.gnu.org/licenses/>.
+ along with Paccoin Ninja.  If not, see <http://www.gnu.org/licenses/>.
 
  */
 
@@ -30,45 +30,87 @@ var pacninjai2p = 'dzjzoefy7fx57h5xkdknikvfv3ckbxu2bx5wryn6taud343g2jma.b32.i2p'
 var pacninjatestneti2p = 'hkttp5yfsmmmtsgynadotlk6t3ppsuaj274jzipj4fe7cko3whza.b32.i2p';
 
 // Coin logos
-var pacninjacoin = ['DASH','tDASH'];
+var pacninjacoin = ['PAC', 'tPAC'];
 
 // URLs
 // Block info
-// ["https://explorer.pacninja.pl/block/%%b%%","elberethzone's Dash Blockchain Explorer"]
-var pacninjablockexplorer = [[["http://chainz.cryptoid.info/dash/block.dws?%%b%%.htm","cryptoID Dash Blockchain Explorer"]],
-                          [["https://test.explorer.pacninja.pl/block/%%b%%","Dash Ninja Testnet Blockchain Explorer"],
-                           ["https://test.insight.dash.siampm.com/block/%%b%%","Alternate Testnet Dash Blockchain Explorer"]]];
+// ["https://explorer.pacninja.pl/block/%%b%%","elberethzone's Paccoin Blockchain Explorer"]
+var pacninjablockexplorer = [
+    [
+        ["http://chainz.cryptoid.info/Paccoin/block.dws?%%b%%.htm", "cryptoID Paccoin Blockchain Explorer"]
+    ],
+    [
+        ["https://test.explorer.pacninja.pl/block/%%b%%", "Paccoin Ninja Testnet Blockchain Explorer"],
+        ["https://test.insight.Paccoin.siampm.com/block/%%b%%", "Alternate Testnet Paccoin Blockchain Explorer"]
+    ]
+];
 
 // Address info
-var pacninjamndetail = [[["/mndetails.html?mnpubkey=%%a%%","Dash Ninja Masternode Detail"],
-                          ["https://www.dashcentral.org/masternodes/%%a%%","Dash Central Masternode Monitoring"]],
-                         [["/mndetails.html?mnpubkey=%%a%%","Dash Ninja Testnet Masternode Detail"]]];
-var pacninjamndetailvin = [[["/mndetails.html?mnoutput=%%a%%","Dash Ninja Masternode Detail"]],
-                            [["/mndetails.html?mnoutput=%%a%%","Dash Ninja Testnet Masternode Detail"]]];
+var pacninjamndetail = [
+    [
+        ["/mndetails.html?mnpubkey=%%a%%", "Paccoin Ninja Masternode Detail"],
+        ["https://www.Paccoincentral.org/masternodes/%%a%%", "Paccoin Central Masternode Monitoring"]
+    ],
+    [
+        ["/mndetails.html?mnpubkey=%%a%%", "Paccoin Ninja Testnet Masternode Detail"]
+    ]
+];
+var pacninjamndetailvin = [
+    [
+        ["/mndetails.html?mnoutput=%%a%%", "Paccoin Ninja Masternode Detail"]
+    ],
+    [
+        ["/mndetails.html?mnoutput=%%a%%", "Paccoin Ninja Testnet Masternode Detail"]
+    ]
+];
 
-var pacninjamndetailprotx = [[["/mndetails.html?protxhash=%%a%%","Dash Ninja Masternode Detail"]],
-                              [["/mndetails.html?protxhash=%%a%%","Dash Ninja Testnet Masternode Detail"]]];
+var pacninjamndetailprotx = [
+    [
+        ["/mndetails.html?protxhash=%%a%%", "Paccoin Ninja Masternode Detail"]
+    ],
+    [
+        ["/mndetails.html?protxhash=%%a%%", "Paccoin Ninja Testnet Masternode Detail"]
+    ]
+];
 
-// ["https://explorer.pacninja.pl/address/%%a%%","elberethzone's Dash Blockchain Explorer"],
-var pacninjaaddressexplorer = [[["https://chainz.cryptoid.info/dash/address.dws?%%a%%.htm","cryptoID Dash Blockchain Explorer"]],
-                                [["https://test.explorer.pacninja.pl/address/%%a%%","Dash Ninja Testnet Blockchain Explorer"],
-                                 ["https://test.insight.dash.siampm.com/address/%%a%%","Alternate Testnet Dash Blockchain Explorer"]]];
-// ["http://explorer.pacninja.pl/tx/%%a%%","elberethzone's Dash Blockchain Explorer"],
-var pacninjatxexplorer = [[["https://chainz.cryptoid.info/dash/tx.dws?%%a%%.htm","cryptoID Dash Blockchain Explorer"]],
-                           [["http://test.explorer.pacninja.pl/tx/%%a%%","Dash Ninja Testnet Blockchain Explorer"],
-                            ["https://test.insight.dash.siampm.com/tx/%%a%%","Alternate Testnet Dash Blockchain Explorer"]]];
+// ["https://explorer.pacninja.pl/address/%%a%%","elberethzone's Paccoin Blockchain Explorer"],
+var pacninjaaddressexplorer = [
+    [
+        ["https://chainz.cryptoid.info/Paccoin/address.dws?%%a%%.htm", "cryptoID Paccoin Blockchain Explorer"]
+    ],
+    [
+        ["https://test.explorer.pacninja.pl/address/%%a%%", "Paccoin Ninja Testnet Blockchain Explorer"],
+        ["https://test.insight.Paccoin.siampm.com/address/%%a%%", "Alternate Testnet Paccoin Blockchain Explorer"]
+    ]
+];
+// ["http://explorer.pacninja.pl/tx/%%a%%","elberethzone's Paccoin Blockchain Explorer"],
+var pacninjatxexplorer = [
+    [
+        ["https://chainz.cryptoid.info/Paccoin/tx.dws?%%a%%.htm", "cryptoID Paccoin Blockchain Explorer"]
+    ],
+    [
+        ["http://test.explorer.pacninja.pl/tx/%%a%%", "Paccoin Ninja Testnet Blockchain Explorer"],
+        ["https://test.insight.Paccoin.siampm.com/tx/%%a%%", "Alternate Testnet Paccoin Blockchain Explorer"]
+    ]
+];
 
 // Search query
-// ["https://explorer.pacninja.pl/search?q=%%q%%","elberethzone's Dash Blockchain Explorer"],
-var pacninjaqueryexplorer = [[["https://chainz.cryptoid.info/dash/search.dws?q=%%q%%","cryptoID Dash Blockchain Explorer"]],
-                            [["https://test.explorer.pacninja.pl/search?q=%%q%%","Dash Ninja Testnet Blockchain Explorer"],
-                             ["http://test.explorer.darkcoin.qa/search?q=%%q%%","Official Testnet Dash Blockchain Explorer"]]];
+// ["https://explorer.pacninja.pl/search?q=%%q%%","elberethzone's Paccoin Blockchain Explorer"],
+var pacninjaqueryexplorer = [
+    [
+        ["https://chainz.cryptoid.info/Paccoin/search.dws?q=%%q%%", "cryptoID Paccoin Blockchain Explorer"]
+    ],
+    [
+        ["https://test.explorer.pacninja.pl/search?q=%%q%%", "Paccoin Ninja Testnet Blockchain Explorer"],
+        ["http://test.explorer.darkcoin.qa/search?q=%%q%%", "Official Testnet Paccoin Blockchain Explorer"]
+    ]
+];
 
-var pacninjamasternodemonitoring = ["/masternodes.html?mnregexp=%%p%%#mnversions","/masternodes.html?mnregexp=%%p%%#mnversions"];
+var pacninjamasternodemonitoring = ["/masternodes.html?mnregexp=%%p%%#mnversions", "/masternodes.html?mnregexp=%%p%%#mnversions"];
 
-var pacninjabudgetdetail = ["/budgetdetails.html?budgetid=%%b%%","/budgetdetails.html?budgetid=%%b%%"];
+var pacninjabudgetdetail = ["/budgetdetails.html?budgetid=%%b%%", "/budgetdetails.html?budgetid=%%b%%"];
 
-var pacninjagovernanceproposaldetail = ["/proposaldetails.html?proposalhash=%%b%%","/proposaldetails.html?proposalhash=%%b%%"];
+var pacninjagovernanceproposaldetail = ["/proposaldetails.html?proposalhash=%%b%%", "/proposaldetails.html?proposalhash=%%b%%"];
 
 // Blocks per day
-var dashblocksperday = 553;
+var Paccoinblocksperday = 553;
